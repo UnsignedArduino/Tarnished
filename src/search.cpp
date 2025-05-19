e1f90a86e159261a4560ea04b5554f301858ca08
BuildTools <anikpatel1322@gmail.com>
2025-05-19 10:53:42 -0400

    bench 4149201

--- src/search.cpp
+++ src/search.cpp
@@ -24,6 +24,9 @@ namespace Search {
 	bool isLoss(int score){
 		return score <= GETTING_MATED;
 	}
+	bool isMateScore(int score){
+		return std::abs(score) >= FOUND_MATE;
+	}
 	void fillLmr(){
 		// Weiss formula for reductions is
 		// Captures/Promo: 0.2 + log(depth) * log(movecount) / 3.35
@@ -309,12 +312,12 @@ namespace Search {
 			// https://github.com/mcthouacbb/Sirius/blob/15501c19650f53f0a10973695a6d284bc243bf7d/Sirius/src/search.cpp#L620
 			bool doSE = !root && moveIsNull(ss->excluded) &&
 						depth >= SE_MIN_DEPTH && ttEntry->move == move && ttEntry->depth >= depth - 3
-						&& ttEntry->flag != TTFlag::FAIL_LOW && ttEntry->score > GETTING_MATED;	
+						&& ttEntry->flag != TTFlag::FAIL_LOW && !isMateScore(ttEntry->score);	
 			
 			int extension = 0;
 
 			if (doSE) {
-				int sBeta = std::max(-INFINITE + 1, ttEntry->score - SE_BETA_SCALE * depth / 16);
+				int sBeta = std::max(-MATE, ttEntry->score - SE_BETA_SCALE * depth / 16);
 				int sDepth = (depth - 1) / 2;
 				// How good are we without this move
 				ss->excluded = ttEntry->move;
